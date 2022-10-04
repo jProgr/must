c00609681efabc5b7249bc57764ccbc9ce33f93c
jProgr <8537691@gmail.com>
2022-10-03 21:57:50 -0500

    Add MustOn
    For flows that need to map a value after a bool check.

--- must.go
+++ must.go
@@ -11,6 +11,19 @@ func Must[T any](f func() (T, error)) T {
     return v
 }
 
+func MustOn[T, U any](
+    value T,
+    evaluator func(value T) bool,
+    mapper func(value T) U,
+    err any,
+) U {
+    if !evaluator(value) {
+        log.Panicln(err)
+    }
+
+    return mapper(value)
+}
+
 func JustDo(f func() error) {
     err := f()
     if err != nil {
